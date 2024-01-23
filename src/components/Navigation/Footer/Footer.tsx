82aa2742089dde7265250bf241c195cfd92e087a
Hexpod604 <hexpod604@gmail.com>
2024-01-23 14:46:07 -0300

    Improved responsive design and alignment

--- src/components/Navigation/Footer/Footer.tsx
+++ src/components/Navigation/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <footer className="bg-black">
-      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8 text-center">
+      <div className="w-full max-w-screen-xl mx-auto p-4 sm:px-0 md:py-8 text-center">
         <div className="flex flex-col sm:flex-row items-center sm:justify-between">
           <Link
             href="#"
