1d141921480e0781caa1bc80498887026787084d
Hexpod604 <hexpod604@gmail.com>
2024-01-24 10:00:15 -0300

    Improve download button

--- src/components/Buttons/DownloadButton.tsx
+++ src/components/Buttons/DownloadButton.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function DownloadButton() {
   return (
-    <Link href="/cv-lautaro-spiazzi.png" target="_BLANK" className="hvr-grow-rotate bg-blue-800 hover:bg-blue-900 text-gray-100 font-semibold px-3 py-2 sm:px-4 rounded inline-flex items-center duration-100">
+    <Link href="https://github.com/Hexpod604/Portfolio-Remaster" target="_BLANK" className="hvr-grow-rotate bg-blue-800 hover:bg-blue-900 text-gray-100 font-semibold px-3 py-2 sm:px-4 rounded inline-flex items-center duration-100">
       <svg
         className="fill-current w-4 h-4 mr-2"
         xmlns="http://www.w3.org/2000/svg"
@@ -12,7 +12,7 @@ export default function DownloadButton() {
       >
         <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
       </svg>
-      <span>Descargar CV</span>
+      <span>Visitar Repositorio</span>
     </Link>
   );
 }
