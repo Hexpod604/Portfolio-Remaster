45c50b5f8db93550612302bd9bb7355cc8062c13
Hexpod604 <hexpod604@gmail.com>
2024-01-24 15:03:11 -0300

    Removed unused code, added AOS init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,17 @@ import Projects from "@/components/Body/Projects";
 import Technologies from "@/components/Body/Technologies";
 import Navbar from "@/components/Navigation/Navbar/Navbar";
 
+import { useEffect } from "react";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 function HomePage() {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+    });
+  }, []);
   return (
     <>
       <header className="bg-black pb-8">
@@ -17,10 +27,8 @@ function HomePage() {
       </header>
       <Header></Header>
 
-      <main className="bg-gradient-to-br from-gray-800 via-gray-900 to-gray-800">
-        {/* <HorizontalRuler></HorizontalRuler> */}
+      <main className="bg-gradient-to-br from-gray-800 via-gray-900 to-gray-800 bg-tech-bg bg-center bg-no-repeat bg-contain">
         <Expositor></Expositor>
-        <HorizontalRuler></HorizontalRuler>
         <Projects></Projects>
         <Technologies></Technologies>
         <Presentation></Presentation>
