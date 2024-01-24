6952b5f58c67fbcb6de3c5011fc03f20e217dafa
Hexpod604 <hexpod604@gmail.com>
2024-01-24 16:14:21 -0300

    Minimalized footer

--- src/components/Navigation/Footer/Footer.tsx
+++ src/components/Navigation/Footer/Footer.tsx
@@ -5,47 +5,9 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <footer className="bg-black">
-      <div className="w-full max-w-screen-xl mx-auto p-4 sm:px-0 md:py-8 text-center">
-        <div className="flex flex-col sm:flex-row items-center sm:justify-between">
-          <Link
-            href="#"
-            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
-          >
-            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
-              Lautaro Spiazzi
-            </span>
-          </Link>
-          <ul className="flex flex-wrap gap-3 justify-center items-center mb-2 text-sm font-medium sm:mb-0 text-gray-400">
-            <li>
-              <Link href="#start" className="hover:underline md:me-6">
-                Inicio
-              </Link>
-            </li>
-            <li>
-              <Link href="#projects" className="hover:underline md:me-6">
-                Proyectos
-              </Link>
-            </li>
-            <li>
-              <Link href="#technologies" className="hover:underline md:me-6">
-                Tecnologías
-              </Link>
-            </li>
-            <li>
-              <Link href="#aboutme" className="hover:underline md:me-6">
-                Sobre mi
-              </Link>
-            </li>
-            <li>
-              <Link href="#contact" className="hover:underline">
-                Contacto
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <hr className="my-6 sm:mx-auto border-gray-700 lg:my-8" />
-        <span className="block text-sm sm:text-center text-gray-400">
+    <footer className="bg-black w-full">
+      <div className="w-full md:max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg 2xl:max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-8 px-4 lg:px-0 text-center">
+        <span className="flex-1 block text-md text-center text-gray-400">
           Desarrollado con ❤ por{" "}
           <Link href="/" className="hover:underline">
             Lautaro Spiazzi
